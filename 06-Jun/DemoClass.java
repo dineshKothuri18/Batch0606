add676c74e2265a629feae2e58963ba148124e9f
fsdbatchjuly <fsdbatchjuly2023@gmail.com>
2024-06-06 12:03:29 +0530

    day1 Java Introduction

---
+++ 06-Jun/DemoClass.java
@@ -0,0 +1,16 @@
+
+public class DemoClass {
+
+
+	public static void main(String[] args){
+		
+		System.out.println("Hello World");
+		System.out.println("Hello Narasimha");
+		System.out.println("Hello Sumanth");
+		System.out.println("Hello Hemanth");
+		System.out.println("Welcome to day 1 class");
+		
+			
+	}
+
+}
