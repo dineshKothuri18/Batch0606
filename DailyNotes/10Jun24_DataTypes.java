fb055f794f7e2b53495961ff751573307853f87a
fsdbatchjuly <fsdbatchjuly2023@gmail.com>
2024-06-10 12:41:57 +0530

    DAtatypes & Variables

---
+++ DailyNotes/10Jun24_DataTypes.java
@@ -0,0 +1,52 @@
+public class DataTypes{
+
+	public static void main(String[] args){
+
+			 
+			float age; //define or declaration 
+			
+			age = 100.24f;// assignment
+
+			System.out.println(age);
+			age  = 200;
+			System.out.println(age);
+
+			String name = "Dinesh"
+			int age = 33;
+			float height = 5.6f;
+
+
+		/*			
+			Primitive Data Types
+			boolean, char, short	, int, long, float double,
+
+			String
+
+	1.Create your Details object in a .jsonFile
+	2.Wap to identify the data types, declare variables and print your
+	bio data
+
+
+			[
+{
+	"name":"Dinesh",
+	"age":33,
+	"height":"5.6",
+	"gender":"M",	
+	"email":"dinesh@gmail.com",
+	"qualification":"BTech",
+	"Mobile":"8142377077",
+	"Address":"Hyderabad",
+
+},
+
+
+
+]
+
+		*/
+	}
+
+
+}
+
