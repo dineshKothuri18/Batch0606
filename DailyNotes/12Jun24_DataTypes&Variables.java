23a568414272c9a728255f0f27f3a30c2f95c5dc
fsdbatchjuly <fsdbatchjuly2023@gmail.com>
2024-06-12 17:25:13 +0530

    Arithmetic operators

---
+++ DailyNotes/12Jun24_DataTypes&Variables.java
@@ -0,0 +1,30 @@
+public class ArithmeticOperators{
+
+	public static void main(String[] args){
+			
+
+			/* + , - , *, /,% 
+
+				variable name can have A-Z,a-z,0-9,_
+			*/
+			
+			int num1 = 7, num2 = 10, sum, sub,mul;
+			float div, mod;
+
+			sum = num1 + num2;
+			sub = num1 - num2;
+			mul = num1 * num2;
+			div = num1/(float)num2;
+
+			System.out.println("Addition is " + sum);
+			System.out.println("Subtraction is " + sub);
+			System.out.println("Multiplication is " + mul);
+			System.out.println("Division is " + div);
+		
+			
+
+	}
+
+}
+
+ 
