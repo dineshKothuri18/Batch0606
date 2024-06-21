5e116215e93bef9283d6d23d2a900c96681b44e2
fsdbatchjuly <fsdbatchjuly2023@gmail.com>
2024-06-21 13:01:08 +0530

    Paysli Task

---
+++ DailyNotes/21PaySlipTask.java
@@ -0,0 +1,9 @@
+Payslip Breakdown:
+Base Salary: 70%
+Employer PF: 9% of Base
+Employee PF: 9% of Base
+HRA : 50% of Base
+LTA : 20%
+Health Insurance : 10%
+
+The Home = Base Salary+HRA+LTA-HI-Employee PF-Employer PF
