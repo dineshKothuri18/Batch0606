dabdefaa03f116cc5c7057e6a9527cd7f3d9ff83
unknown <chaitanyalellapalli@gmail.com>
2024-06-27 14:26:57 +0530

    chaitanya added file

---
+++ StudentTaskFiles/Chaitanya/StudentDetails.java
@@ -0,0 +1,228 @@
+public class StudentDetails
+{
+ public static void main(String[] args)
+{
+        String name= "Narasimha A";
+      System.out.print(name);
+        int computer= 85;
+        int drawing= 78;
+        int m1= 88;
+        int m2= 92;
+        int m3= 80;
+        int m4= 76;
+        int physics= 81;
+        int chemistry= 79;
+        int english=98;
+        int totalstudentmarks = computer+drawing+m1+m2+m3+m4+physics+chemistry+english;
+        System.out.println("total marks of a student:+totalstudentmarks");
+        float percentage = (((float)totalstudentmarks/900)*100);
+        System.out.println("percentage:"+percentage);
+     
+    
+   
+      name= "Hemanth Ch";
+       computer= 90;
+        drawing= 82;
+        m1= 86;
+        m2= 89;
+        m3= 84;
+        m4= 77;
+        physics= 83;
+        chemistry= 78;
+        english= 85;
+          totalstudentmarks = computer+drawing+m1+m2+m3+m4+physics+chemistry+english;
+        System.out.println("total marks of a student:+totalstudentmarks");
+         percentage = (((float)totalstudentmarks/900)*100);
+        System.out.println("percentage:"+percentage);
+
+    
+      name="Sumanth M";
+       computer= 75;
+        drawing= 80;
+        m1= 79;
+        m2= 85;
+        m3= 88;
+        m4= 82;
+        physics=77;
+        chemistry= 80;
+        english= 79;
+         totalstudentmarks = computer+drawing+m1+m2+m3+m4+physics+chemistry+english;
+        System.out.println("total marks of a student:+totalstudentmarks");
+         percentage = (((float)totalstudentmarks/900)*100);
+        System.out.println("percentage:"+percentage);
+
+
+      name= "Mohith Sai K";
+        computer=82;
+        drawing= 77;
+        m1= 83;
+        m2= 87;
+        m3= 81;
+        m4= 78;
+        physics= 80;
+        chemistry= 85;
+        english=82;
+          totalstudentmarks = computer+drawing+m1+m2+m3+m4+physics+chemistry+english;
+        System.out.println("total marks of a student:+totalstudentmarks");
+         percentage = (((float)totalstudentmarks/900)*100);
+        System.out.println("percentage:"+percentage);
+
+      name= "Vijaya Lakshmi P";
+        computer=88;
+        drawing=84;
+        m1= 85;
+        m2= 90;
+        m3= 89;
+        m4= 81;
+        physics= 82;
+        chemistry= 87;
+        english=86;
+          totalstudentmarks = computer+drawing+m1+m2+m3+m4+physics+chemistry+english;
+        System.out.println("total marks of a student:+totalstudentmarks");
+        percentage = (((float)totalstudentmarks/900)*100);
+        System.out.println("percentage:"+percentage);
+
+   
+      name= "Kavya Sri K";
+        computer= 79;
+        drawing =75;
+        m1 =80;
+        m2= 83;
+        m3= 79;   
+        m4=76;
+        physics =78;
+        chemistry= 81;
+        english=80;
+        totalstudentmarks = computer+drawing+m1+m2+m3+m4+physics+chemistry+english;
+        System.out.println("total marks of a student:+totalstudentmarks");
+        percentage = (((float)totalstudentmarks/900)*100);
+        System.out.println("percentage:"+percentage);
+
+   
+      name ="Akhila D";
+        computer=84;
+        drawing=79;
+        m1= 82;
+        m2= 85;
+        m3= 86;
+        m4= 80;
+        physics= 79;
+        chemistry= 83;
+        english= 81;
+         totalstudentmarks = computer+drawing+m1+m2+m3+m4+physics+chemistry+english;
+        System.out.println("total marks of a student:+totalstudentmarks");
+        percentage = (((float)totalstudentmarks/900)*100);
+        System.out.println("percentage:"+percentage);
+
+
+      name= "Sai Charan P";
+        computer= 76;
+        drawing= 81;
+        m1= 77;
+        m2= 84;
+        m3 =80;
+        m4= 82;
+        physics= 78;
+        chemistry= 79;
+        english= 80;
+        totalstudentmarks = computer+drawing+m1+m2+m3+m4+physics+chemistry+english;
+        System.out.println("total marks of a student:+totalstudentmarks");
+        percentage = (((float)totalstudentmarks/900)*100);
+        System.out.println("percentage:"+percentage);
+
+        name= "Sathvika A";
+        computer= 89;
+        drawing= 85;
+        m1= 87;
+        m2= 90;
+        m3= 84;
+        m4= 88;
+        physics= 86;
+        chemistry= 86;
+        english= 87;
+         totalstudentmarks = computer+drawing+m1+m2+m3+m4+physics+chemistry+english;
+        System.out.println("total marks of a student:+totalstudentmarks");
+         percentage = (((float)totalstudentmarks/900)*100);
+        System.out.println("percentage:"+percentage);
+
+     
+      name= "Karishma Md";
+        computer= 77;
+        drawing=80;
+        m1=78;
+        m2= 83;
+        m3= 81;
+        m4= 79;
+        physics= 76;
+        chemistry= 80;
+        english=78;
+         totalstudentmarks = computer+drawing+m1+m2+m3+m4+physics+chemistry+english;
+        System.out.println("total marks of a student:+totalstudentmarks");
+         percentage = (((float)totalstudentmarks/900)*100);
+        System.out.println("percentage:"+percentage);
+
+    
+      name="Aparna K";
+        computer= 83;
+        drawing=79;
+        m1=82;
+        m2= 88;
+        m3= 80;
+        m4= 85;
+        physics= 81;
+        chemistry= 84;
+        english=83;
+         totalstudentmarks = computer+drawing+m1+m2+m3+m4+physics+chemistry+english;
+        System.out.println("total marks of a student:+totalstudentmarks");
+         percentage = (((float)totalstudentmarks/900)*100);
+        System.out.println("percentage:"+percentage);
+
+    
+      name= "Lakshmi Chaitanya L";
+        computer= 85;
+        drawing= 81;
+        m1= 84;
+        m2= 89;
+        m3= 82;
+        m4= 86;
+        physics= 83;
+        chemistry= 87;
+        english=85;
+         totalstudentmarks = computer+drawing+m1+m2+m3+m4+physics+chemistry+english;
+        System.out.println("total marks of a student:+totalstudentmarks");
+         percentage = (((float)totalstudentmarks/900)*100);
+        System.out.println("percentage:"+percentage);
+
+     name= "Rahul ";
+        computer= 85;
+        drawing= 81;
+        m1= 84;
+        m2= 89;
+        m3= 82;
+        m4= 86;
+        physics= 83;
+        chemistry= 87;
+        english=85;
+         totalstudentmarks = computer+drawing+m1+m2+m3+m4+physics+chemistry+english;
+        System.out.println("total marks of a student:+totalstudentmarks");
+         percentage = (((float)totalstudentmarks/900)*100);
+        System.out.println("percentage:"+percentage);
+      
+
+        name= "Sai kumar J";
+    
+        computer= 86;
+        drawing= 86;
+        m1= 83;
+        m2= 85;
+        m3= 80;
+        m4= 88;
+        physics= 83;
+        chemistry= 87;
+        english=85;
+         totalstudentmarks = computer+drawing+m1+m2+m3+m4+physics+chemistry+english;
+        System.out.println("total marks of a student:+totalstudentmarks");
+         percentage = (((float)totalstudentmarks/900)*100);
+        System.out.println("percentage:"+percentage);
+}
+}
