2e2ee3d8f7d0f657ad2f03f2e49ea746a5000fe0
fsdbatchjuly <fsdbatchjuly2023@gmail.com>
2024-07-01 12:40:39 +0530

    Loops while,dowhile,for

---
+++ DailyNotes/1JulLoops.java
@@ -0,0 +1,53 @@
+
+	public class Loops {
+	
+		public static void main(String[] args){	
+
+
+
+			String[] studentNames = {"Sumanth","Sai","Rahul","Raghu"};
+	
+			for(int i=0;i<=3;i++){
+				System.out.println(studentNames[i]);
+				
+			}
+			
+			
+			
+
+			for(int i = 1; i<=5 ; i++){	
+				System.out.println(i+".Hi People -- for loop");
+			}		
+
+			int loopWhile = 11;
+			while(loopWhile <=10){
+				System.out.println(loopWhile +".Hi People -- While loop");
+			}
+			
+			*		write a programme to take 2 numbers as input and print its even, odd, prime number between them
+		also print num1 Table upto num2
+
+		Output:	
+		Please Enter number1 : 3
+		Please Enter number2 : 30
+		Even numbers between 3-30 are : .........
+		Odd numbers between 3-30 are : .........
+		Prime numbers between 3-30 are : .........
+		Table is
+			3*1 = 3
+			3*2 = 6
+			3*3 = 9
+			.
+			.
+			.
+			3*30 = 90
+*/		
+
+		if user entered num1>num2  print please enter valid numbers and ask the user again
+		Please Enter number1 : 3
+		Please Enter number2 : 30
+	}
+
+
+		}	
+	}
