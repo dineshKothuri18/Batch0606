610a167384a6aa69631faf15baa3a808b36f0d6a
SUMANTH MENA <menasumanth@gmail.com>
2024-07-01 14:16:14 +0530

    Change

--- StudentTaskFiles/Sumanth/Even.java
+++ StudentTaskFiles/Sumanth/Even.java
@@ -2,27 +2,29 @@ import java.util.Scanner;
 public class Even{
 	public static void main(String[] args){
                	
-		Scanner sc = new Scanner(System.in);
-		System.out.println("enter a number : ");
-		int number1 = sc.nextInt();
-		System.out.println("Enter a limit  : ");
-		int number2 = sc.nextInt();   
+		int number1=0;
+		int number2=0;
+		while(number1>=number2){ 
+               Scanner sc = new Scanner(System.in);
+		System.out.print("enter a number1 : ");
+		number1=sc.nextInt();
+		System.out.print("enter a number2 : ");
+		number2=sc.nextInt();
+	 System.out.println("enter correct numbers ");
+	}		 
 	 System.out.print("Even numbers between " + number1 + "-" + number2 + " are : ");
-         for (int i = number1; i <= number2; i++) 
+         for (int i = number1+1; i <= number2-1; i++) 
             if (i % 2 == 0) 
               System.out.print(i + " ");
 
            	System.out.print("\nodd numbers between " + number1 + "-" + number2 + " are : ");
-        for (int i = number1; i <= number2; i++) 
+        for (int i = number1+1; i <= number2-1; i++) 
             if (i %2 != 0) 
                 System.out.print(i + " ");
 	
 			System.out.println("\nMultiplication of " +number1+"*"+number2 + ":");
 		for (int i = 1; i <= number2; i++)  
             		System.out.println(number1+ "*" +i + "="+(number1*i));
-
-
     
-       
-}
+     }
 }
