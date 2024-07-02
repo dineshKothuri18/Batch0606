9252ef26d656efa912452f0b744ba954055b856b
suvarchala99 <96271544+suvarchala99@users.noreply.github.com>
2024-07-02 19:11:12 +0530

    Update Studdetails.java

--- StudentTaskFiles/Suvarchala/Studdetails.java
+++ StudentTaskFiles/Suvarchala/Studdetails.java
@@ -7,7 +7,7 @@ public class Studdetails {
         int[] html_marks = {98, 90, 80, 70, 20, 30, 30, 80, 50, 20, 60, 80, 50, 40, 44, 55, 7, 77, 8};
         int[] sql_marks = {97, 70, 80, 60, 20, 30, 30, 80, 50, 60, 60, 90, 40, 40, 49, 54, 8, 57, 6};
         int[] javascript_marks = {96, 90, 80, 50, 40, 30, 20, 70, 80, 60, 50, 40, 20, 20, 14, 75, 60, 77, 5};
-        System.out.printf("%-10s %-10s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %5s %-5s%n", "Name", "Roll No", "Batch", "Java", "HTML", "SQL", "JS", "sum", "Avg", "Min", "Max", "Percent","pass/fail");
+        System.out.printf("%-10s %-10s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %5s %-15s%n", "Name", "Roll No", "Batch", "Java", "HTML", "SQL", "JS", "sum", "Avg", "Min", "Max", "Percent","pass/fail");
             for(int i=0;i<stname.length;i++) {
                 int d=Math.min(java_marks[i],html_marks[i]);
                 int e=Math.min(sql_marks[i],javascript_marks[i]);
@@ -20,10 +20,10 @@ public class Studdetails {
                 float percent=(((float)sumofmarkse)*100)/400;
                 if(java_marks[i]>=35 && html_marks[i]>=35 && sql_marks[i]>=35 && javascript_marks[i]>=35) {
 
-                    System.out.printf("%-10s %-10s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %5s     %-5s%n", stname[i], rollnumbers[i], batchname[i], java_marks[i], html_marks[i], sql_marks[i], javascript_marks[i], sumofmarkse, averageofmarkse, mini, maxi, percent,"pass");
+                    System.out.printf("%-10s %-10s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s     %-15s%n", stname[i], rollnumbers[i], batchname[i], java_marks[i], html_marks[i], sql_marks[i], javascript_marks[i], sumofmarkse, averageofmarkse, mini, maxi, percent,"pass");
                 }
                 else{
-                    System.out.printf("%-10s %-10s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %5s     %-5s%n", stname[i], rollnumbers[i], batchname[i], java_marks[i], html_marks[i], sql_marks[i], javascript_marks[i], sumofmarkse, averageofmarkse, mini, maxi, percent,"fail");
+                    System.out.printf("%-10s %-10s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s %-5s     %-15s%n", stname[i], rollnumbers[i], batchname[i], java_marks[i], html_marks[i], sql_marks[i], javascript_marks[i], sumofmarkse, averageofmarkse, mini, maxi, percent,"fail");
                 }
             }
 
