597bb0108cefb688db0ba8dba351079c0736edfd
saikumar290 <saikumarjonnalagadda37@gmail.com>
2024-07-05 11:48:58 +0530

    changes

--- StudentTaskFiles/Sai Kumar/Laddu.java
+++ StudentTaskFiles/Sai Kumar/Laddu.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Laddu{
 	public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter a  number : ");
+	System.out.println("Enter a  number solution you want: ");
 	int a = sc.nextInt();
-	for (int i=3;i<=a;i+=3){
+	int i=3;
+	while(a>0){
 		if (((27*i+38)%8)==0){
 			System.out.println(((27*i+38)/8));
+a--;
 }
+		i+=3;
 
-}
-		
+}		
 }
 }
