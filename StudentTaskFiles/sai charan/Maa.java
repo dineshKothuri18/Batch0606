4ee5aa0acff3306952c0e901ba7bb303d712f5b6
Sai Charan <pathemsaicharan@gmail.com>
2024-07-05 10:35:38 +0530

    changes

--- StudentTaskFiles/sai charan/Maa.java
+++ StudentTaskFiles/sai charan/Maa.java
@@ -7,7 +7,7 @@ public class Maa
 		String word = sc.nextLine();
 		for (int i = 0; i < word.length(); i++){
 		    int count = 0;
-		    for(int j = i; j < word.length();j++){go
+		    for(int j = i; j < word.length();j++){
 		        if(word.charAt(i) == (word.charAt(j))){
 		            count++;
 		        }
