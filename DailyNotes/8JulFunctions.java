2d3b46646c7e8f70a1ac603816068f19729e5139
fsdbatchjuly <fsdbatchjuly2023@gmail.com>
2024-07-08 12:07:00 +0530

    Task added

--- DailyNotes/8JulFunctions.java
+++ DailyNotes/8JulFunctions.java
@@ -37,9 +37,27 @@ public class FunctionsTopic{
 		int c = a + b;
 		return c;
 	}
-
-
-
 	
 
 }
+
+/*
+Task
+
+	1. Wirte a program and show the below options 
+		1.Arithematic Calculations
+		2.Check a number is Prime/not
+		3.Factors	of a number
+		4.Letters count of a string
+		5.End
+	 Write a program to take two numbers input and print its addition,subtraction,multiplication
+		(create function for addition, sub,mult)
+		
+	Display the options again after the output
+*/
+
+
+
+
+
+
