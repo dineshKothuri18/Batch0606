cf4c511bc0f6a3c357a5f7f211e3915b8e8bb756
fsdbatchjuly <fsdbatchjuly2023@gmail.com>
2024-07-08 11:56:12 +0530

    Functions

---
+++ DailyNotes/8JulFunctions.java
@@ -0,0 +1,45 @@
+public class FunctionsTopic{
+
+	public static void main(String[] args){
+		
+			
+	
+
+		
+	}
+
+	public static void additionOfTwoNumbers(){
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+	}
+
+
+	public static void additionOfTwoNumbers(int a, int b) -> {
+		
+		int c = a + b;
+	}
+
+
+
+	public static int additionOfTwoNumbers(){
+		
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+
+		return c;
+	}
+
+
+	public static int additionOfTwoNumbers(int a, int b){
+				
+		int c = a + b;
+		return c;
+	}
+
+
+
+	
+
+}
