5de7e5f42de78bbbe4a863eff58a4f85be93936e
vamsi <vamsi93923@email.com>
2024-07-16 19:13:47 +0530

    vamsi

--- StudentTaskFiles/Vamsi krishna/JavaCourse/vamsi/src/HashMaps.java
+++ StudentTaskFiles/Vamsi krishna/JavaCourse/vamsi/src/HashMaps.java
@@ -4,14 +4,14 @@ public class HashMaps {
     public static HashMap<String,Integer>fooditems;
     public static void main(String[] args)
     {
-            fooditems =new HashMap<>();
-               fooditems.put("Dosa",45);
-               fooditems.put("Idly",25);
-               fooditems.put("Puri",40);
-               fooditems.put("Pongal",45);
-               fooditems.put("Vada",30);
-               fooditems.put("Bonda",40);
-               fooditems.put("Omelet",20);
+        fooditems =new HashMap<>();
+        fooditems.put("Dosa",45);
+        fooditems.put("Idly",25);
+        fooditems.put("Puri",40);
+        fooditems.put("Pongal",45);
+        fooditems.put("Vada",30);
+        fooditems.put("Bonda",40);
+        fooditems.put("Omelet",20);
         System.out.println("Total number of items:"+fooditems.size());
         for (String name: fooditems.keySet()){
             System.out.println(name+"-"+fooditems.get(name));
