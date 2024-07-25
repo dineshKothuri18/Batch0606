451287e1eae8b655a624dff61129ba1bf5685ca7
20KE1A0545 <131330790+20KE1A0545@users.noreply.github.com>
2024-07-25 21:50:06 +0530

    added files

---
+++ StudentTaskFiles/Chaitanya/Simple.java
@@ -0,0 +1,23 @@
+public class Simple {
+
+    public String Name;
+    public int Cost;
+    public String Discount;
+    public String Color;
+    public String Barcode;
+
+
+
+
+
+    public static  void Start(){
+        System.out.println("Start");
+    }
+
+    public static void end(){
+        System.out.println("end");
+
+    }
+
+}
+
