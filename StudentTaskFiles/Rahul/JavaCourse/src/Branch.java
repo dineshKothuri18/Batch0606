b8736e5bd92fd90d94fcc1dc6b1cba8eb099641a
Rahul1328 <142076516+Rahul1328@users.noreply.github.com>
2024-07-28 22:04:46 +0530

    Rahul

---
+++ StudentTaskFiles/Rahul/JavaCourse/src/Branch.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Branch {
+    public String Branch_name;
+    public int IFSC_code;
+    public String location;
+    public ArrayList<Employee> employees;
+}
