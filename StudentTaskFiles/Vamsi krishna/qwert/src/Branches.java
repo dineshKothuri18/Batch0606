76f4802036df113366c5054a527d4657ac939c21
Vamsi-63 <vamsi93923@gmail.com>
2024-07-29 11:19:44 +0530

    Bank task

---
+++ StudentTaskFiles/Vamsi krishna/qwert/src/Branches.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+public class Branches {
+    String branchName;
+    String address;
+    String ifscCode;
+    int totalNoOfEmployes;
+ArrayList<AccountHolder>   AccountHolders=new ArrayList<>();
+ArrayList<Staff> staff=new ArrayList<>();
+
+
+}
+
+
