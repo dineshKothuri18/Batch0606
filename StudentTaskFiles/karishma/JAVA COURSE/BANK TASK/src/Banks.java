329a72f20030a6bc66d2398a7be73fe18d209281
MD.karishma <mdkarishu786@gmail.com>
2024-07-29 10:47:30 +0530

    BANK CONCEPT

---
+++ StudentTaskFiles/karishma/JAVA COURSE/BANK TASK/src/Banks.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+    public class Banks {
+        String bankname;
+        int bankcount;
+        ArrayList<Branch> branches = new ArrayList<>();
+
+
+    }
+
+
+
+
