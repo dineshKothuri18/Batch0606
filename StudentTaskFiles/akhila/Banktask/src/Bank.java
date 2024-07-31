1e37ec255cd2bb2335bfb2d3c91686b01dc471cb
Akhila1234-design <akhiladasireddy1434@gmail.com>
2024-07-31 21:52:14 +0530

    bank task

---
+++ StudentTaskFiles/akhila/Banktask/src/Bank.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Bank {
+    String bankname;
+    ArrayList<Branch>  branches = new ArrayList<Branch>();
+     public Bank(String bankname) {
+        this.bankname = bankname;
+
+     }
+}
