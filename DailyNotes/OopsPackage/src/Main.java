76248c47448d320b39fb98475b2906bd7d2e8712
DineshKothuri <dinesh.kothuri18@gmail.com>
2024-08-05 11:52:31 +0530

    Packages

---
+++ DailyNotes/OopsPackage/src/Main.java
@@ -0,0 +1,21 @@
+import com.banks.employee.Employee;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee();
+        emp1.name = "Java";
+        emp1.email = "Java";
+        emp1.employeeId = 123;
+
+        Employee.mySampleFunction();
+
+//        public,private,protected,static,
+        //HomeWork
+        //final,super,Exception Handling
+
+
+    }
+}
