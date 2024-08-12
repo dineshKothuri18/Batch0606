000b003c9fce40312a20709260049cb8fccddbe8
DineshKothuri <dinesh.kothuri18@gmail.com>
2024-08-12 11:52:04 +0530

    Encapsulation

---
+++ DailyNotes/Encapsulation12Aug/src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        Person sai = new Person("Sai",10,"asdjbaksjbdkj");
+        System.out.println(sai.getAge());
+
+        Person vamshi = new Person("Vamsh",11,"asdjbaksjbdkj");
+        System.out.println(vamshi.getAge());
+
+        Person raghu = new Person("Vamsh",12,"asdjbaksjbdkj");
+        System.out.println(raghu.getAge());
+
+
+    }
+}
